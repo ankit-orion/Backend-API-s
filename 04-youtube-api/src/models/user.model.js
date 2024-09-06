50ba0808692f22efd9dd39f301c6d1cae599e985
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-09-06 20:50:33 +0530

    chore: Update user model
    This commit adds jwt and bcrypt.

---
+++ 04-youtube-api/src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose, {Schema} from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        // this is for indexing the username field
+        // it helps in searching the username faster
+        // because it creates a tree like structure
+        index: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName:{
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar:{
+        type: String, // cloudinary url will be stored here
+        required: true,
+    },
+    coverImage:{
+        type: String, // cloudinary url will be stored here
+    },
+    watchHistory:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Video'
+        }
+    ],
+    likedVideos:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Video'
+        }
+    ],
+    password:{
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refreshToken:{
+        type: String
+    },
+
+},{timestamps: true});
+
+// this method should called only when the password is being set
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {_id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {_id: this._id},
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+// jwt is a bearer token which is used for authentication
+// it acts as a proof that the user is authenticated
+
+
+export const User = mongoose.model('User', userSchema);
---
+++ 04-youtube-api/src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; 
+
+const videoSchema = new Schema({
+    videoFile:{
+        type: String, // cloudinary url will be stored here
+        required: true,
+    },
+    thumbnail:{
+        type: String, // cloudinary url will be stored here
+        required: true,
+    },
+    title:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    duration:{
+        type: Number, // cloudinary will provide the duration of the video
+        required: true,
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    likes:{
+        type: Number,
+        default: 0
+    },
+    isPublised:{
+        type: Boolean,
+        default: true,
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+}, {timestamps: true})
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model('Video', videoSchema);
