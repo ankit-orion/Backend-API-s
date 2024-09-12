e635a04a1e6c3d50f9e3f17bc10116b3587d6258
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-09-12 19:55:15 +0530

    configuring cloduinary.js for file uploading

---
+++ 04-youtube-api/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'; // fs is a Node.js module for reading and writing files
+
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+// here we are setting up the file path for local file
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+        })
+        // file has been uploaded on cloudinary
+        // console.log('File uploaded successfully on cloudinary', response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath); // delete the file from local storage in case we were not able to upload it on cloudinary
+        console.error('Error uploading file on cloudinary', error);
+        return null;
+    }
+}
+
+export default uploadOnCloudinary;
